0b10581f4ff2a7b5012ba3f833d3412cce6c4638
melvintan1985mm <melvintan1985.mm@gmail.com>
2017-09-23 16:51:53 +0800

    Update demoClass.java

--- test/src/test/demoClass.java
+++ test/src/test/demoClass.java
@@ -4,11 +4,7 @@ public class demoClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		if(true){
-			System.out.println("true");
-		} elseif (false) {
-			System.out.println("false");
-		}
+	
 		
 		
 	}
